97dd225f40db0ec0c7fe155c62a6655ecd036a68
XanaDevelops <danielgv.space@gmail.com>
2024-04-18 11:40:40 +0200

    Fix merging

--- app/src/main/java/com/example/moonword/MainActivity.java
+++ app/src/main/java/com/example/moonword/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.moonword;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        int interficie = View.SYSTEM_UI_FLAG_IMMERSIVE;
+        decorView.setSystemUiVisibility(interficie);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+    public void setLletra(View v){
+        Button btn =  (Button) v;
+        String lletra = btn.getText().toString();
+        System.out.println("D: lletra:"+lletra);
+        btn.setEnabled(!btn.isEnabled());
+    }
+
+    public void btnRandom(View v){
+        Button btn = (Button)v;
+        System.out.println("D: pulsado Random");
+    }
+
+    public void btnBonus(View v){
+        Button btn = (Button)v;
+        System.out.println("D: pulsado bonus");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(" < encertades i possibles >");
+        builder.setMessage(" < la llista de trobades >");
+        // Un botó OK per tancar la finestra
+        builder.setPositiveButton(" OK ",null);
+        // Mostrar l ’ AlertDialog a la pantalla
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
