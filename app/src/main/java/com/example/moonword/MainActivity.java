4e51bcd50587ceefa416e6a95778d82f882c209d
XanaDevelops <danielgv.space@gmail.com>
2024-04-18 11:07:13 +0200

    Codigo placeholder botones

--- app/src/main/java/com/example/moonword/MainActivity.java
+++ app/src/main/java/com/example/moonword/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.moonword;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setLletra(View v){
+        Button btn =  (Button) v;
+        String lletra = btn.getText().toString();
+        System.out.println("D: lletra:"+lletra);
+        btn.setEnabled(!btn.isEnabled());
+    }
+
+    public void btnRandom(View v){
+        Button btn = (Button)v;
+        System.out.println("D: pulsado Random");
+    }
+
+    public void btnBonus(View v){
+        Button btn = (Button)v;
+        System.out.println("D: pulsado bonus");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(" < encertades i possibles >");
+        builder.setMessage(" < la llista de trobades >");
+        // Un botó OK per tancar la finestra
+        builder.setPositiveButton(" OK ",null);
+        // Mostrar l ’ AlertDialog a la pantalla
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
