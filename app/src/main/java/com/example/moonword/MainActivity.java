75a462087ba6e7409db3ccb86e9c380a67dda192
Daniel GV <danielgv.space@gmail.com>
2024-04-22 18:11:58 +0200

    funciona varios, falta centrar

--- app/src/main/java/com/example/moonword/MainActivity.java
+++ app/src/main/java/com/example/moonword/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.moonword;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.annotation.IdRes;
 import androidx.annotation.IntRange;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,6 +9,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.app.AlertDialog;
+import android.graphics.fonts.FontStyle;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(interficie);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
-        crearFilaTextViews(R.id.ref15H, 5);
+        crearFilaTextViews(R.id.ref15H, 7);
     }
 
     public void setLletra(View v){
@@ -40,25 +43,33 @@ public class MainActivity extends AppCompatActivity {
         TextView textViews[] = new TextView[lletres];
 
 
-        int lastTextViewId = guia; // La primera vista debe estar debajo de la Guideline
+        int lastTextViewId = -1;
 
         // Iterar para crear y posicionar cada TextView
         for (int i = 0; i < lletres; i++) {
             TextView textView = new TextView(this);
             textView.setId(View.generateViewId()); // Generar ID único para el TextView
             int id = textView.getId();
-            textView.setText("HOLA: "+i);
+            textView.setText(""+i);
+            textView.setTextSize(32);
 
             layout.addView(textView);
 
             ConstraintSet constraintSet = new ConstraintSet();
             constraintSet.clone(layout);
-            constraintSet.constrainWidth(id, 300);
-            constraintSet.constrainHeight(id, 200);
+            constraintSet.constrainWidth(id, 60);
+            constraintSet.constrainHeight(id, 120);
             constraintSet.connect(id, ConstraintSet.TOP, guia, ConstraintSet.BOTTOM, 10);
+            if(i>=1){
+                constraintSet.connect(id, ConstraintSet.LEFT, lastTextViewId, ConstraintSet.RIGHT, 10);
+            }else{
+
+            }
+            //constraintSet.centerHorizontally(id, ConstraintSet.PARENT_ID);
             constraintSet.applyTo(layout);
 
             textViews[i] = textView;
+            lastTextViewId = id;
         }
 
         return textViews;
