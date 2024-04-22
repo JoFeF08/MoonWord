9102f1c3d1f2920da1ee03c526b0c9ba0651f50f
Daniel GV <danielgv.space@gmail.com>
2024-04-22 18:49:39 +0200

    placeholder esParaulaSolucio()

---
+++ app/src/main/java/com/example/moonword/Game.java
@@ -0,0 +1,12 @@
+package com.example.moonword;
+
+public class Game {
+
+    /**
+     * static de moment, idea de instanciar Game, per joc
+     */
+    public static boolean esParaulaSolucio(String p1, String p2){
+        return false;
+    }
+
+}
--- app/src/main/java/com/example/moonword/GameLogic.java
+++
@@ -1,4 +0,0 @@
-package com.example.moonword;
-
-public class GameLogic {
-}
