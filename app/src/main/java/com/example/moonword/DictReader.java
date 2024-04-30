fab48739cb768c0517114d9c9476ee1987b51de6
XanaDevelops <danielgv.space@gmail.com>
2024-04-30 08:37:46 +0200

    loadAll, falta implementar maps

--- app/src/main/java/com/example/moonword/DictReader.java
+++ app/src/main/java/com/example/moonword/DictReader.java
@@ -8,11 +8,17 @@ import androidx.annotation.NonNull;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import SetMap.Map.MapInterficie;
 
 
 public class DictReader {
 
     private static BufferedReader reader;
+
+    private static MapInterficie<Integer, ArrayList<String>> mapaNum;
+    private static MapInterficie<String, String> mapaWord;
 
     /**
      *
@@ -20,6 +26,26 @@ public class DictReader {
      */
     public static void configure(@NonNull Context context){
         reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.paraules)));
+    }
+
+    /**
+     * no se si al final ha de ser aixi
+     * @throws IOException
+     */
+    public static void loadAll() throws IOException {
+        while (reader.ready()){
+            String line = reader.readLine();
+            String[] words = line.split(";");
+            mapaWord.put(words[0], words[1]);
+            int len = words[0].length();
+            if(mapaNum.get(len)==null){
+                ArrayList<String> aux = new ArrayList<>();
+                aux.add(words[0]);
+                mapaNum.put(len, aux);
+            }else{
+                mapaNum.get(len).add(words[0]);
+            }
+        }
     }
 
 
