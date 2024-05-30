1d5d6f52392a809df96a387ea461930ca5ecd71d
JoFeF08 <135724351+JoFeF08@users.noreply.github.com>
2024-05-30 16:20:46 +0200

    bonus escriure

--- app/src/main/java/com/example/moonword/MainActivity.java
+++ app/src/main/java/com/example/moonword/MainActivity.java
@@ -255,12 +255,14 @@ public class MainActivity extends AppCompatActivity {
     private void startGame(){
         int carryBonus;
         if (this.currentGame == null){
-            carryBonus=0;
+            carryBonus = 0;
         }else{
             carryBonus = this.currentGame.getContadorBonus();
         }
         this.currentGame = new Game(Game.random.nextInt(4)+4); //entre 7 y 3
+
         this.currentGame.setContadorBonus(carryBonus);
+        bonusButton.setText(carryBonus + "");
 
         Log.i("START_GAME()", currentGame.toString());
 
